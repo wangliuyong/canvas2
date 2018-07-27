24e04bb571bd6223bb8b9f22bd8b52518b10b7b9
wangliuyong <1355498705@qq.com>
2018-07-27 14:05:01 +0800

    兼容触屏设备

--- js/main.js
+++ js/main.js
@@ -1,25 +1,17 @@
 var canvas = byId("canvas");
-var box = byId("box");
-var eraserButton = byId("eraser");
-var penButton = byId("pen");
 var context = canvas.getContext('2d');
 var usering = false;
 var eraser = false;
-/******************************画布大小**************************************** */
+
+
+/*1.画布大小 */
 setCanvasSize(canvas);
-/****************************鼠标活动********************************* */
-listernMouse(canvas);
-/**************************橡皮擦************************************************ */
-penButton.onclick=function(){
-    eraser=true;
-    box.className="box2";
-}
-eraserButton.onclick = function() {
-  eraser = false;
-  box.className = "box1";
-};
+/*2鼠标活动 */
+listernUser(canvas);
+/*3.橡皮擦画笔切换 */
+switchPen();
 /************************************** 封装的函数 *********** */
-//
+//1.通过id获取元素
 function byId(id) {
     var element=undefined;
     if (document.getElementById(id)){
@@ -29,24 +21,7 @@ function byId(id) {
     }
     return element;
 }
-
-//画圆
-var drawCircle = function (x, y, radius) {
-    context.beginPath();
-    context.arc(x, y, radius, Math.PI * 2, false);
-    context.stroke();
-};
-//画线 
-var drawLine = function (x1, y1, x2, y2) {
-    context.beginPath();
-    context.lineWidth = 1;
-    context.moveTo(x1, y1);
-    context.lineTo(x2, y2);
-    context.stroke();
-    context.closePath();
-}
-
-//设置画布尺寸
+//2.设置画布尺寸
 function setCanvasSize(canvas) {
     //视口尺寸改变事件
     window.onresize = function () {
@@ -58,46 +33,106 @@ function setCanvasSize(canvas) {
     }
     window.onresize();
 }
-//
-function listernMouse(canvas) {
+//3.用户动作监听
+function listernUser(canvas) {
     var point1 = {
         x: undefined,
         y: undefined
     };
-    canvas.onmousedown = function (event) {
-        var x = event.clientX,
-            y = event.clientY;
+    if (isTouchDevice()) {
+      // on Mobile
+      canvas.ontouchstart = function(event) {
+        var x = event.touches[0].clientX,
+          y = event.touches[0].clientY;
         usering = true;
         if (eraser) {
-            context.clearRect(x - 5, y - 5, 10, 10);
+          context.clearRect(x - 10, y - 10, 20, 20);
         } else {
-            //drawCircle(x, y, 10);
-            point1 = {
-                x: x,
-                y: y
-            };
+          //drawCircle(x, y, 20);
+          point1 = { x: x, y: y };
         }
-    }
-    canvas.onmousemove = function () {
-        var x = event.clientX,
-            y = event.clientY;
-        var point2 = {
-            x: x,
-            y: y
-        };
+      };
+      canvas.ontouchmove = function() {
+        var x = event.touches[0].clientX,
+          y = event.touches[0].clientY;
+        var point2 = { x: x, y: y };
         if (usering) {
-            if (usering) {
-                if (eraser){
-                    context.clearRect(x - 5, y - 5, 10, 10);
-                } else {
-                    //drawCircle(x, y, 10);
-                    drawLine(point1.x, point1.y, point2.x, point2.y);
-                    point1 = point2 //实时更新，上一个点
-                } 
-            } 
-        } 
-    }
-    canvas.onmouseup = function () {
+          if (eraser) {
+            context.clearRect(x - 10, y - 10, 20, 20);
+          } else {
+            //drawCircle(x, y, 20);
+            drawLine(point1.x, point1.y, point2.x, point2.y);
+            point1 = point2; //实时更新，上一个点
+          }
+        }
+      };
+      canvas.ontouchend = function() {
         usering = false;
+      };
+    } else {
+      // on Desktop
+      canvas.onmousedown = function(event) {
+        var x = event.clientX,
+          y = event.clientY;
+        usering = true;
+        if (eraser) {
+          context.clearRect(x - 10, y - 10, 20, 20);
+        } else {
+          //drawCircle(x, y, 20);
+          point1 = { x: x, y: y };
+        }
+      };
+      canvas.onmousemove = function() {
+        var x = event.clientX,
+          y = event.clientY;
+        var point2 = { x: x, y: y };
+        if (usering) {
+          if (eraser) {
+            context.clearRect(x - 10, y - 10, 20, 20);
+          } else {
+            //drawCircle(x, y, 20);
+            drawLine(point1.x, point1.y, point2.x, point2.y);
+            point1 = point2; //实时更新，上一个点
+          }
+        }
+      };
+      canvas.onmouseup = function() {
+        usering = false;
+      };
     }
 }
+//4.特性检测
+function isTouchDevice() {
+    return "ontouchstart" in document.documentElement;
+}
+
+//10.画圆
+function drawCircle(x, y, radius) {
+    context.beginPath();
+    context.arc(x, y, radius, Math.PI * 2, false);
+    context.stroke();
+};
+//6.画线 
+function drawLine(x1, y1, x2, y2) {
+    context.beginPath();
+    context.lineWidth = 5;
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+    context.stroke();
+    context.closePath();
+}
+//7.画笔橡皮擦的切换
+function switchPen() {
+    var box = byId("box");
+    var eraserButton = byId("eraser");
+    var penButton = byId("pen");
+    penButton.onclick = function () {
+        eraser = true;
+        box.className = "box2";
+    }
+    eraserButton.onclick = function () {
+        eraser = false;
+        box.className = "box1";
+    };
+}
+
